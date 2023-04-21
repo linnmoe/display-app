e51e9e0b27f4ce7dd5c952c36332ee47fdef540d
linn moestam <linn.moestam@knowit.se>
2023-04-21 11:00:47 +0200

    clock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Grid container spacing={2}>
+        <Grid item xs={6} md={8}>
+          Event
+          <ul>
+            <li>
+              AW-kväll
+            </li>
+            <li>
+              Docker-kurs
+            </li>
+          </ul>
+        </Grid>
+        <Grid item xs={6} md={4}>
+          Klocka
+          <Clock />
+        </Grid>
+        <Grid item xs={6} md={4}>
+          Tidtabeller
+        </Grid>
+        <Grid item xs={6} md={8}>
+
+        </Grid>
+      </Grid>
+
     </div>
   );
 }
---
+++ src/Clock.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+function Clock() {
+  return (
+    <div>
+     <h1>dfsdf</h1>
+    </div>
+  );
+}
+
+export default Clock;
